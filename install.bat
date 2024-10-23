cd48b89790ff9a9b95082a860c6aeed246b1c85c
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2024-10-23 13:11:13 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,9 +1,18 @@
 @echo off
 echo Installing...
 
+rem Make sure the application folder exists, copy the code over to it.
 if not exist "C:\Program Files\PasswordChanger\" (
   mkdir "C:\Program Files\PasswordChanger"
 )
-
 copy app.py "C:\Program Files\PasswordChanger"
 xcopy /s /y templates "C:\Program Files\PasswordChanger"
+
+rem Change to the application folder.
+cd "C:\Program Files\PasswordChanger"
+
+rem Make sure the Python Virtual Environment (venv) exists.
+if not exist "venv\" (
+  pip install virtualenv
+  virtualenv venv
+)
