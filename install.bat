f96f0e2bbdac37e78a12b86597d8749ccbd1610f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2024-10-23 14:55:44 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -11,6 +11,7 @@ xcopy /s /y templates "C:\Program Files\PasswordChanger\templates"
 
 rem Change to the application folder.
 cd "C:\Program Files\PasswordChanger"
+set srcFolder=%cd%
 
 rem Make sure the Python Virtual Environment (venv) exists.
 if not exist "venv\" (
@@ -19,7 +20,16 @@ if not exist "venv\" (
 )
 
 rem Make sure the Python module requirements are installed in the venv.
-venv\Scripts\pip.exe install flask
-venv\Scripts\pip.exe install waitress
+if not exist "C:\Program Files\PasswordChanger\venv\Lib\site-packages\flask" (
+  venv\Scripts\pip.exe install flask
+)
+if not exist "C:\Program Files\PasswordChanger\venv\Lib\site-packages\waitress" (
+  venv\Scripts\pip.exe install waitress
+)
 
-rem venv\Scripts\python.exe app.py
+echo Setting up PasswordChanger (running via the Waitress WSGI server) as a Windows service...
+%srcPath%\nssm\2.24\win64\nssm install PasswordChanger "C:\Program Files\PasswordChanger\runWaitress.bat" > nul 2>&1
+%srcPath%\nssm\2.24\win64\nssm set PasswordChanger DisplayName "Password Changer" > nul 2>&1
+%srcPath%\nssm\2.24\win64\nssm set PasswordChanger AppNoConsole 1 > nul 2>&1
+%srcPath%\nssm\2.24\win64\nssm set PasswordChanger Start SERVICE_AUTO_START > nul 2>&1
+net start PasswordChanger
