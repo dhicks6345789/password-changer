ce5a8a65f8e6c4776b9e3b0b727e8dda29cb8507
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2024-10-23 15:01:49 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -30,8 +30,9 @@ if not exist "C:\Program Files\PasswordChanger\venv\Lib\site-packages\waitress" 
 
 echo Setting up PasswordChanger (running via the Waitress WSGI server) as a Windows service...
 echo bananas > nul 2>&1
-"%srcPath%\nssm\2.24\win64\nssm" install PasswordChanger "C:\Program Files\PasswordChanger\runWaitress.bat"
-"%srcPath%\nssm\2.24\win64\nssm" set PasswordChanger DisplayName "Password Changer"
-"%srcPath%\nssm\2.24\win64\nssm" set PasswordChanger AppNoConsole 1
-"%srcPath%\nssm\2.24\win64\nssm" set PasswordChanger Start SERVICE_AUTO_START
+echo "%srcPath%\nssm\2.24\win64\nssm.exe" install PasswordChanger "C:\Program Files\PasswordChanger\runWaitress.bat"
+"%srcPath%\nssm\2.24\win64\nssm.exe" install PasswordChanger "C:\Program Files\PasswordChanger\runWaitress.bat"
+"%srcPath%\nssm\2.24\win64\nssm.exe" set PasswordChanger DisplayName "Password Changer"
+"%srcPath%\nssm\2.24\win64\nssm.exe" set PasswordChanger AppNoConsole 1
+"%srcPath%\nssm\2.24\win64\nssm.exe" set PasswordChanger Start SERVICE_AUTO_START
 net start PasswordChanger
