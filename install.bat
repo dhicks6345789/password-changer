3b82417acbe3d971423ba4822f0fb4ef93a78f0c
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2024-10-23 13:50:45 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,9 +4,10 @@ echo Installing...
 rem Make sure the application folder exists, copy the code over to it.
 if not exist "C:\Program Files\PasswordChanger\" (
   mkdir "C:\Program Files\PasswordChanger"
+  mkdir "C:\Program Files\PasswordChanger\templates"
 )
 copy app.py "C:\Program Files\PasswordChanger"
-xcopy /s /y templates "C:\Program Files\PasswordChanger"
+xcopy /s /y templates "C:\Program Files\PasswordChanger\templates"
 
 rem Change to the application folder.
 cd "C:\Program Files\PasswordChanger"
