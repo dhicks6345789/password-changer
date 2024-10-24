cd6e74a1a24ddc1d6a1b6c082cc7f15f3afb86b8
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2024-10-24 13:43:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+# Standard libraries.
 import flask
 import json
+
+# Libraries for handling Google OAuth (i.e. user sign-in) authentication flow.
+import google.oauth2.id_token
+import google.auth.transport.requests
 
 app = flask.Flask(__name__)
 configError = ""
@@ -29,11 +34,27 @@ def index():
   else:
     return flask.render_template("error.html", appData=appData, errorMessage=configError)
 
-@app.route("/api/authenticateGoogleCredential", methods=["POST"])
-def authenticateGoogleCredential():
-  aud = flask.request.values.get("aud", None)
-  print(aud)
-  return("authCalled")
-
+@app.route("/api/verifyGoogleIDToken", methods=["POST"])
+def verifyGoogleIDToken():
+    googleIDToken = flask.request.values.get("googleIDToken", None)
+    try:
+        # See for further details: https://developers.google.com/identity/gsi/web/guides/verify-google-id-token
+        IDInfo = google.oauth2.id_token.verify_oauth2_token(googleIDToken, google.auth.transport.requests.Request(), googleClientID)
+        
+        # If the issuer isn't Google, there's a problem.
+        if IDInfo["iss"] not in ["accounts.google.com", "https://accounts.google.com"]:
+            raise ValueError("Wrong issuer.")
+            
+        # The aud value should match the client ID.
+        if not IDInfo["aud"] == googleClientID:
+            raise ValueError("Mismatched client ID.")
+    except ValueError as e:
+        # Invalid token.
+        return "Error - " + repr(e)
+    # At this point, we've verified the Google login token. Generate and cache a login token for our client-side code to use.
+    #loginToken = generateLoginToken({"emailAddress":IDInfo["email"], "loginType":"google"})
+    loginToken = "12345678"
+    return(loginToken)
+  
 if __name__ == "__main__":
   app.run(debug=True, port=8070)
