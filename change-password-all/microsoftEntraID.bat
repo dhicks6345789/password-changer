64e92ddefebec6268b75e9761da4f85cead3d889
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-01-16 13:26:33 +0000

    Create microsoftEntraID.bat

---
+++ change-password-all/microsoftEntraID.bat
@@ -0,0 +1,4 @@
+@echo off
+echo %1 %2
+rem Set-EntraUserPassword [-ForceChangePasswordNextLogin <Boolean>] [-EnforceChangePasswordPolicy <Boolean>] -UserId <String> -Password <SecureString> [<CommonParameters>]
+exit 1
