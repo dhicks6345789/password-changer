f9cf1e2d4b17c6f94a778fb0faa54335ffc1ceca
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-01-16 16:29:20 +0000

    Update microsoftEntraID.ps1

--- change-password-all/microsoftEntraID.ps1
+++ change-password-all/microsoftEntraID.ps1
@@ -1,3 +1,4 @@
 param ([string]$UserID, [string]$Password)
 $securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+Connect-Entra -Scopes 'User.ReadWrite.All'
 Set-EntraUserPassword -UserId $UserID -Password $securePassword
