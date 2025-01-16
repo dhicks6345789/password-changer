51dd984936452ff70478e0f5190381ac984bfc43
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-01-16 17:16:21 +0000

    Update microsoftEntraID.ps1

--- change-password-all/microsoftEntraID.ps1
+++ change-password-all/microsoftEntraID.ps1
@@ -1,9 +1,12 @@
 param ([string]$UserID, [string]$Password)
+$securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+
+$accessToken = Get-Content .\MSGraphAccessToken.txt -Raw
+echo $accessToken
+$secureAccessToken = ConvertTo-SecureString $accessToken -AsPlainText -Force
 
 #Connect-Entra -Scopes 'User.ReadWrite.All'
-$accessToken = Get-Content .\MSGraphAccessToken.txt -Raw
-Connect-MgGraph -AccessToken ($accessToken | ConvertTo-SecureString -AsPlainText -Force) -Scopes 'User.ReadWrite.All'
+Connect-MgGraph -AccessToken $secureAccessToken -Scopes 'User.ReadWrite.All'
 
-$securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
 Set-EntraUserPassword -UserId $UserID -Password $securePassword
 exit 1
