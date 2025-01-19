400bdf1a8a8b44afe450efe24ebc1f0e4b5a0427
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-01-19 16:42:51 +0000

    Update microsoftEntraID.ps1

--- change-password-all/microsoftEntraID.ps1
+++ change-password-all/microsoftEntraID.ps1
@@ -6,10 +6,12 @@ $clientID = Get-Content .\MSGraphClientID.txt -Raw
 $certificateThumbprint = Get-Content .\MSGraphCertificateThumbprint.txt -Raw
 Connect-MgGraph -ClientId $clientID -TenantId $tenantID -CertificateThumbprint $certificateThumbprint -NoWelcome
 
-Set-EntraUserPassword -UserId $UserID -Password $securePassword -ErrorAction SilentlyContinue
+#Set-EntraUserPassword -UserId $UserID -Password $securePassword -ErrorAction SilentlyContinue
+Set-EntraUserPassword -UserId $UserID -Password $securePassword -ErrorVariable setPasswordError
 
 echo "Bananas!"
+#echo $error.Status
 
-echo $error.Status
+echo $setPasswordError
 
 exit 1
