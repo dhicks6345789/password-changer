ab9dc98362ee67e8b2a93e0c38780b7387ad633a
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-01-19 15:52:48 +0000

    Update microsoftEntraID.ps1

--- change-password-all/microsoftEntraID.ps1
+++ change-password-all/microsoftEntraID.ps1
@@ -10,7 +10,7 @@ $securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
 $tenantID = Get-Content .\MSGraphTenantID.txt -Raw
 $clientID = Get-Content .\MSGraphClientID.txt -Raw
 $certificateThumbprint = Get-Content .\MSGraphCertificateThumbprint.txt -Raw
-Connect-MgGraph -ClientId $clientID -TenantId $tenantID -CertificateThumbprint $certificateThumbprint
+Connect-MgGraph -ClientId $clientID -TenantId $tenantID -CertificateThumbprint $certificateThumbprint -NoWelcome
 
 Set-EntraUserPassword -UserId $UserID -Password $securePassword
 exit 1
