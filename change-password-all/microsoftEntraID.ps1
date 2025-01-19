3a85474b1b82c6d26be926eb847b87743e6a897a
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-01-19 16:20:29 +0000

    Update microsoftEntraID.ps1

--- change-password-all/microsoftEntraID.ps1
+++ change-password-all/microsoftEntraID.ps1
@@ -1,16 +1,17 @@
 param ([string]$UserID, [string]$Password)
 $securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
 
-#$accessToken = Get-Content .\MSGraphAccessToken.txt -Raw
-#$secureAccessToken = ConvertTo-SecureString $accessToken -AsPlainText -Force
-
-#Connect-Entra -Scopes 'User.ReadWrite.All'
-#Connect-MgGraph -AccessToken $secureAccessToken -NoWelcome
-
 $tenantID = Get-Content .\MSGraphTenantID.txt -Raw
 $clientID = Get-Content .\MSGraphClientID.txt -Raw
 $certificateThumbprint = Get-Content .\MSGraphCertificateThumbprint.txt -Raw
 Connect-MgGraph -ClientId $clientID -TenantId $tenantID -CertificateThumbprint $certificateThumbprint -NoWelcome
 
-Set-EntraUserPassword -UserId $UserID -Password $securePassword
+try {
+  Set-EntraUserPassword -UserId $UserID -Password $securePassword
+} catch (Exception) {
+  echo "Bananas!"
+}
+
+#} catch [Request_ResourceNotFound] {
+
 exit 1
