710f0b19644ac98ce8959298f66612a3f320e3e1
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-01-21 16:36:27 +0000

    Update microsoftEntraID.ps1

--- change-password-all/microsoftEntraID.ps1
+++ change-password-all/microsoftEntraID.ps1
@@ -8,14 +8,13 @@ Connect-MgGraph -ClientId $clientID -TenantId $tenantID -CertificateThumbprint $
 
 Set-EntraUserPassword -UserId $UserID -Password $securePassword -ErrorAction SilentlyContinue
 if ($? -eq $false) {
-  echo "Error setting password"
+  echo "Error setting password - trying to create user first.
+  $PasswordProfile = @{
+    Password = $Password
+  }
+  #DisplayName = "John Smith"
+  #MailNickName = left of @
+  #New-MgUser -DisplayName $UserID -PasswordProfile $PasswordProfile -AccountEnabled -MailNickName $UserID -UserPrincipalName $UserID
 }
-
-#echo $error.Status
-
-echo "Oranges!"
-echo $setPasswordError
-echo "Bananas!"
-echo $setPasswordError.Status
 
 exit 1
