6ac7cb079afd14b8ce04959477edaf7e8a709860
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-01-22 18:46:42 +0000

    Update sendEmail.ps1

--- change-password-all/sendEmail.ps1
+++ change-password-all/sendEmail.ps1
@@ -3,4 +3,4 @@ param ([string]$UserID, [string]$Password)
 $destinationEmailAddress = Get-Content .\NotificationsToEmailAddress.txt -Raw
 $fromEmailAddress = Get-Content .\NotificationsFromEmailAddress.txt -Raw
 
-gam sendemail $destinationEmailAddress from $fromEmailAddress mailbox $fromEmailAddress subject "User $UserID: Password Change" message "User $UserID has had their password changed by the password changer utility."
+gam sendemail $destinationEmailAddress from $fromEmailAddress mailbox $fromEmailAddress subject "User $UserID Password Change" message "User $UserID has had their password changed by the password changer utility."
