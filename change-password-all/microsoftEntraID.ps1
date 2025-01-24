00bbd4b26d6ca99deb858f17b7c4a4e05cf205f7
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-01-24 13:31:11 +0000

    Update microsoftEntraID.ps1

--- change-password-all/microsoftEntraID.ps1
+++ change-password-all/microsoftEntraID.ps1
@@ -6,23 +6,18 @@ $clientID = Get-Content .\MSGraphClientID.txt -Raw
 $certificateThumbprint = Get-Content .\MSGraphCertificateThumbprint.txt -Raw
 Connect-MgGraph -ClientId $clientID -TenantId $tenantID -CertificateThumbprint $certificateThumbprint -NoWelcome
 
-# Set-EntraUserPassword -UserId $UserID -Password $securePassword -ErrorAction SilentlyContinue
 Try {
   Set-EntraUserPassword -UserId $UserID -Password $securePassword -ErrorAction Stop
 } Catch [Exception] {
-  echo "An Exception was caught!"
-}
-echo "Error state:"
-echo $?
-if ($? -eq $false) {
   echo "Error setting password - trying to create user first."
   $PasswordProfile = @{
     Password = $Password
   }
   #DisplayName = "John Smith"
   $MailNickName = $UserID.Split("@")[0]
-  New-MgUser -DisplayName $MailNickName -PasswordProfile $PasswordProfile -AccountEnabled -MailNickName $MailNickName -UserPrincipalName $UserID
-  if ($? -eq $false) {
+  Try {
+    New-MgUser -DisplayName $MailNickName -PasswordProfile $PasswordProfile -AccountEnabled -MailNickName $MailNickName -UserPrincipalName $UserID -ErrorAction Stop
+  } Catch [Exception] {
     echo "Error creating Entra ID user: $UserID."
     exit 1
   }
